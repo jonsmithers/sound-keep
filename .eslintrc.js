ba3a23b7ed7af8bb7128edead36232cb15cf4497
Jon Smithers <mail@jonsmithers.link>
2018-03-11 15:19:21 -0400

    Autoformat eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "plugins": [
-        "html"
-    ],
-    "env": {
-        "browser": true,
-        "node":    false,
-        "es6":     true
-    },
-    "parserOptions": {
-      "sourceType": "module",
-      "ecmaVersion": 2017
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent":                [ "warn", 2        ],
-        "linebreak-style":       [ "warn", "unix"   ],
-        "no-console":            [ "off", {allow: ["warn", "error"]} ],
-        "no-constant-condition": [ "warn", {checkLoops: false}],
-        "no-unused-vars":        [ "warn", {vars: "local"} ],
-        "quotes":                [ "off",  "double" ],
-        "semi":                  [ "warn", "always" ]
-    }
+  "plugins": [
+    "html"
+  ],
+  "env": {
+    "browser": true,
+    "node":    false,
+    "es6":     true
+  },
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 2017
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent":                [ "warn", 2        ],
+    "linebreak-style":       [ "warn", "unix"   ],
+    "no-console":            [ "off", {allow: ["warn", "error"]} ],
+    "no-constant-condition": [ "warn", {checkLoops: false}],
+    "no-unused-vars":        [ "warn", {vars: "local"} ],
+    "quotes":                [ "off",  "double" ],
+    "semi":                  [ "warn", "always" ]
+  }
 };
