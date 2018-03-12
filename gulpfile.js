e0a115ea85640e9b70c9fe2641854c9beea84afe
Jon Smithers <mail@jonsmithers.link>
2018-03-12 00:02:24 -0400

    Iron out deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp   = require('gulp');
 var deploy = require('gulp-deploy-git');
  
 gulp.task('deploy', function() {
-  return gulp.src('./**/*')
+  return gulp.src(['./**/*', "!./gulpfile.js"], {read: false})
     .pipe(deploy({
-      repository: 'https://github.com/zhevron/gulp-deploy-git.git',
+      repository: 'https://github.com/jonsmithers/sound-keep.git',
       remoteBranch: 'gh-pages'
     }));
 });
