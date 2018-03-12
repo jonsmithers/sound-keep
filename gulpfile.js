7e502bd9d47edd113d07f156fea98910077e6979
Jon Smithers <mail@jonsmithers.link>
2018-03-12 00:12:31 -0400

    Ironed out deploy task
    Needed .nojekyll for reasons described here:
    
    https://www.bennadel.com/blog/3181-including-node-modules-and-vendors-folders-in-your-github-pages-site.htm

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp   = require('gulp');
 var deploy = require('gulp-deploy-git');
  
 gulp.task('deploy', function() {
-  return gulp.src(['./**/*', "!./gulpfile.js"], {read: false})
+  return gulp.src(['.nojekyll', './**/*', "!./gulpfile.js", "!./.gitignore"], {read: false})
     .pipe(deploy({
       repository: 'https://github.com/jonsmithers/sound-keep.git',
       remoteBranch: 'gh-pages'
